47e97c37f3e989f6085a172326f8ac0dc021141e
EnGenius <atiqqq0@gmail.com>
2024-07-22 18:48:59 +0100

    updated AWS IAM S3 configuration and updated menu Items page

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -1,36 +1,25 @@
 import { useState } from "react";
 import EditableImage from "./EditableImage";
 
+import MenuItemPriceProps from "./MenuItemPriceProps";
+
 export default function MenuItemForm({onSubmit, menuItem}) {
     const [image, setImage] = useState(menuItem?.image || '');
     const [name, setName] = useState(menuItem?.name || '');
     const [description, setDescription] = useState(menuItem?.description || '');
     const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '');
     // State to manage the list of pizza sizes
-    const [sizes, setSizes] = useState([menuItem?.sizes || []]);
+    const [sizes, setSizes] = useState(menuItem?.sizes || []);
+    const [toppingsPrice,setToppingsPrice] = useState(menuItem?.toppingsPrice || [])
 
-    function addSize() {
-        setSizes(oldSizes =>{
-            return [...oldSizes, {name:'', price:0}];
-        });
-    }
-
-    function editSize(ev, index, prop) {
-        const newValue=ev.target.value;
-        setSizes(prevSizes => {
-            const newSizes = [...prevSizes];
-            newSizes[index][prop] = newValue;
-            return newSizes;
-        });
-    }
-
-    function removeSize(indexToBeRemoved) {
-        setSizes(prev =>prev.filter((v,index) => index !== indexToBeRemoved));
-    }
 
     return (
         <form 
-            onSubmit={ev =>onSubmit(ev,{image, name, description, basePrice, sizes})} 
+            onSubmit={ev =>
+                onSubmit(ev,{
+                    image, name, description, basePrice, sizes,toppingsPrice
+                })
+            } 
             className="mt-8 max-w-md mx-auto">
                 <div
                     className="grid items-start gap-4"
@@ -57,45 +46,18 @@ export default function MenuItemForm({onSubmit, menuItem}) {
                             value={basePrice}
                             onChange={ev =>setBasePrice(ev.target.value)}
                         />
-                        <div className="bg-gray-200 p-2 rounded-md mb-2">
-                            <label>Choose Size</label>
-                            {sizes?.length > 0 && sizes.map(size, index =>(
-                                <div className="flex gap-2 items-end">
-                                    <div>
-                                    <label>Size</label>
-                                        <input 
-                                            type="text" 
-                                            placeholder="Size name" 
-                                            value={size.name}
-                                            onChange={ev => editSize(ev, index, 'name')}
-                                        />
-                                    </div>
-                                    <div>
-                                        <label>Price</label>
-                                        <input className="rounded-md"
-                                            type="text" 
-                                            placeholder="price" 
-                                            value={size.price}
-                                            onChange={ev => editSize(ev, index, 'name')}
-                                        />
-                                    </div>
-                                    <div>
-                                        <button 
-                                            type="button"
-                                            className="bg-white mb-2"
-                                            onClick={() =>removeSize(index)}
-                                        >X
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                            <button 
-                                type="button"
-                                onClick={addSize}
-                                className="bg-white">
-                                Add size(M/L)
-                            </button>
-                        </div>
+                        <MenuItemPriceProps
+                            title={'Size'} 
+                            name={'Size'} 
+                            addLabel={'Add size(M/L)'}
+                            props={sizes} 
+                            setProps={setSizes}/>
+                        <MenuItemPriceProps
+                            title={'Toppings'}
+                            name={'Extra Toppings'}
+                            addLabel={'Add toppings'}
+                            props={toppingsPrice}
+                            setProps={setToppingsPrice}/>
                         <button type="submit">Save</button>
                     </div>
                 </div>
---
+++ src/components/layout/MenuItemPriceProps.js
@@ -0,0 +1,81 @@
+import Delete from "../icons/Delete";
+import Add from "../icons/Add";
+import ChevronDown from "../icons/ChevronDown";
+import { useState } from "react";
+import ChevronUp from "../icons/ChevronUp";
+
+export default function MenuItemPriceProps({title, name,addLabel,props,setProps}){
+
+    const [toggleOn,setToggleOn] = useState(false);
+
+    function addProps() {
+        setProps(oldProps =>{
+            return [...oldProps, {name:'', price:0}];
+        });
+    }
+
+    function editProps(ev, index, prop) {
+        const newValue=ev.target.value;
+        setProps(prevSizes => {
+            const newSizes = [...prevSizes];
+            newSizes[index][prop] = newValue;
+            return newSizes;
+        });
+    }
+
+    function removeProps(indexToBeRemoved) {
+        setProps(prev =>prev.filter((v,index) => index !== indexToBeRemoved));
+    }
+     return(
+        <div className="bg-gray-200 p-2 rounded-md mb-2">
+                <button
+                    className="inline-flex p-1 border-0 justify-start"
+                    type="button">
+                    {toggleOn && (
+                        <ChevronDown/>
+                    )}
+                    {!toggleOn && (
+                        <ChevronUp/>
+                    )}
+                    <span>{title}</span>
+                </button>
+            
+            {props?.length > 0 && props.map((size, index) =>(
+                <div className="flex gap-2 items-end">
+                    <div>
+                    <label>{name}</label>
+                        <input 
+                            type="text" 
+                            placeholder="Size name" 
+                            value={size.name}
+                            onChange={ev => editProps(ev, index, 'name')}
+                        />
+                    </div>
+                    <div>
+                        <label>Price</label>
+                        <input className="rounded-md"
+                            type="text" 
+                            placeholder="price" 
+                            value={size.price}
+                            onChange={ev => editProps(ev, index, 'price')}
+                        />
+                    </div>
+                    <div>
+                        <button 
+                            type="button"
+                            className="bg-white mb-2 px-2"
+                            onClick={() =>removeProps(index)}
+                        ><Delete/>
+                        </button>
+                    </div>
+                </div>
+            ))}
+            <button 
+                type="button"
+                onClick={addProps}
+                className="bg-white flex items-center">
+                <Add/><span>{addLabel}</span>
+            </button>
+        </div>
+     );
+}
